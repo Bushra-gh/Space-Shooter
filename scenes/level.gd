8303fa0f2fd3699114e08c6f0e37b43884194bff
Bushra Rahman <bushrarahman@Bushras-MacBook-Air.local>
2026-06-17 18:00:24 +0530

    added stars

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,6 +2,21 @@ extends Node2D
 
 var meteor_scene: PackedScene = load("res://scenes/meteor.tscn")
 var laser_scene: PackedScene = load("res://scenes/laser.tscn")
+
+func _ready() -> void:
+	var size := get_viewport().get_visible_rect().size
+	var rng := RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		var random_x = rng.randi_range(0,size.x)
+		var random_y = rng.randi_range(0,size.y)
+		star.position = Vector2(random_x, random_y)
+
+		var random_scale = rng.randf_range(1,2)
+		star.scale = Vector2(random_scale,random_scale)
+		
+		star.speed_scale = rng.randf_range(0.6,1.4)
+		
+		
 func _on_meteor_timer_timeout() -> void:
 	var meteor = meteor_scene.instantiate()
 	
